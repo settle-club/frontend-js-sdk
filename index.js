0d733f3f8b273df6670dae68ff722c34a2ad9eeb
jainrithik <rithikjain@gofynd.com>
2024-09-27 10:59:32 +0530

    Add 'close' and 'reload' message types to handle modal interactions

--- index.js
+++ index.js
@@ -65,10 +65,13 @@
       if (type === "success") {
         onSuccess(transaction);
         close();
-        window.removeEventListener("message", handleMessage);
       } else if (type === "failure") {
         onFailure(transaction);
         close();
+      } else if (type === "close") {
+        close();
+      } else if (type === "reload") {
+        window.location.reload();
       }
     }
 
